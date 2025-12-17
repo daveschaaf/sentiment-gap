65246cfa5d29076fa91029f891a531f5291e0c01
David Schaaf <davidschaaf@berkeley.edu>
2025-12-17 10:31:34 -0500

    wip: processing reviews data

--- src/data_loader.py
+++ src/data_loader.py
@@ -4,8 +4,26 @@ import json
 
 REVIEWS_RAW = 'data/raw/Health_and_Personal_Care.jsonl.gz'
 
-def load_reviews(path, limit=10000):
-    df = pd.read_json(path, lines=True, compression="gzip", nrows=limit)
+def load_raw_jsonl(file_path, limit=10000):
+    df = pd.read_json(file_path, lines=True, compression="gzip", nrows=limit)
     return df
 
-# dict_keys(['rating', 'title', 'text', 'images', 'asin', 'parent_asin', 'user_id', 'timestamp', 'helpful_vote', 'verified_purchase'])
+def process_reviews(file_name, limit):
+    df = load_raw_jsonl(REVIEWS_RAW, limit=limit)
+    data_type_map = {
+        'rating': 'float32',
+        'title': 'string',
+        'text': 'string',
+        'asin': 'category',
+        'parent_asin': 'category',
+        'user_id': 'string',
+        'helpful_vote': 'int32', 
+        'verified_purchase': 'bool'
+    }
+    df = df[list(data_type_map.keys())]
+    df = df.astype({k: v for k, v in data_type_map.items()})
+    if 'timestamp' in df.columns:
+        df['timestamp'] = pd.to_datetime(df['timestamp'], unit='ms')
+
+    df.to_csv(file_name)
+    return df
--- test/test_data_loader.py
+++ test/test_data_loader.py
@@ -1,13 +1,24 @@
-from src.data_loader import load_reviews, REVIEWS_RAW
+from src.data_loader import load_raw_jsonl, REVIEWS_RAW, process_reviews
+import os
 
-def test_load_reviews():
+
+def test_load_raw_jsonl():
     assert REVIEWS_RAW == 'data/raw/Health_and_Personal_Care.jsonl.gz'
     limit = 10
-    df = load_reviews(REVIEWS_RAW, limit=limit)
+    df = load_raw_jsonl(REVIEWS_RAW, limit=10)
     expected_columns = ['rating', 'title', 'text', 'images', 'asin', 'parent_asin', 'user_id', 'timestamp', 'helpful_vote', 'verified_purchase']
     df_columns = df.columns
     for column in expected_columns:
         assert column in df_columns
+
+    # df = load_reviews(REVIEWS_RAW, limit = 500000)
+    # assert len(df) == 494121
+def test_process_reviews():
+    processed_file = "data/processed/test_health_and_personal_care.csv"
+    df = process_reviews(processed_file, 10)
     assert len(df) == 10
+    assert 'images'not in df.columns
+    assert os.path.exists(processed_file)
+    os.remove(processed_file)
 
 
