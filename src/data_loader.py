da31cd97bd3f5fc1a62c76ef3d81358eea5124bc
David Schaaf <davidschaaf@berkeley.edu>
2025-12-17 12:47:30 -0500

    Processing and merging meta and review data

--- src/data_loader.py
+++ src/data_loader.py
@@ -1,14 +1,14 @@
 import pandas as pd
 import numpy as np
+from pathlib import Path
 
-REVIEWS_RAW = 'data/raw/Health_and_Personal_Care.jsonl.gz'
-
-def load_raw_jsonl(file_path, limit=10000):
+def load_raw(file_name, limit=9999999):
+    file_path = Path("data", 'raw', file_name)
     df = pd.read_json(file_path, lines=True, compression="gzip", nrows=limit)
     return pd.DataFrame(df)
 
 def process_reviews(file_name, limit):
-    df: pd.DataFrame = load_raw_jsonl(REVIEWS_RAW, limit=limit)
+    df: pd.DataFrame = load_raw(file_name, limit=limit)
 
     # White list columns and set data types
     data_type_map = {
@@ -34,5 +34,41 @@ def process_reviews(file_name, limit):
     df['title'] = df['title'].fillna('No Title')
     df['helpful_vote'] = df['helpful_vote'].fillna(0)
 
-    df.to_csv(file_name)
+    raw_meta_df = load_raw(f"meta_{file_name}")
+    meta_df = clean_metadata(raw_meta_df) 
+    # Join with meta file
+    df = df.merge(meta_df[['parent_asin', "product_title", "description", "features", 'product_listing']], on='parent_asin', how= 'left' )
+
+    processed_file_path = Path('data', 'processed', file_name)
+    df.to_csv(processed_file_path)
     return df
+
+def clean_metadata(meta_df):
+
+    meta_data_types = {
+        'parent_asin': 'category',
+        'title': 'string',
+        'description': 'string', 
+        'features': 'string'
+    }
+    meta_df = meta_df.dropna(subset=['parent_asin'])
+    meta_df = meta_df.loc[:, list(meta_data_types.keys())]
+
+    # convert lists to string
+    for col in ['description', 'features']:
+        meta_df[col] = meta_df[col].apply(lambda c: " ".join(c) if isinstance(c, list) else c)
+    meta_df = meta_df.astype({k: v for k, v in meta_data_types.items()})
+
+    # clean HTMl of tags
+    meta_df['description'] = meta_df['description'].replace(r'<[^>]*>', '', regex=True)
+
+    # Fill NA values with blank
+    meta_df['description'].fillna('')
+    meta_df['title'].fillna('')
+    meta_df['features'].fillna('')
+
+    meta_df['product_listing'] = f"""{meta_df['title']} {meta_df['description']} {meta_df['features']}""".strip()
+
+    meta_df = meta_df.rename(columns={'title': 'product_title'})
+
+    return pd.DataFrame(meta_df)
--- test/test_data_loader.py
+++ test/test_data_loader.py
@@ -1,11 +1,10 @@
-from src.data_loader import load_raw_jsonl, REVIEWS_RAW, process_reviews
+from src.data_loader import load_raw, process_reviews, clean_metadata
 import os
 
-
-def test_load_raw_jsonl():
-    assert REVIEWS_RAW == 'data/raw/Health_and_Personal_Care.jsonl.gz'
+health_reviews = 'Health_and_Personal_Care.jsonl.gz'
+def test_load_raw():
     limit = 10
-    df = load_raw_jsonl(REVIEWS_RAW, limit=10)
+    df = load_raw(health_reviews, limit=limit)
     expected_columns = ['rating', 'title', 'text', 'images', 'asin', 'parent_asin', 'user_id', 'timestamp', 'helpful_vote', 'verified_purchase']
     df_columns = df.columns
     for column in expected_columns:
@@ -15,14 +14,25 @@ def test_load_raw_jsonl():
     # assert len(df) == 494121
 def test_process_reviews():
     processed_file = "data/processed/test_health_and_personal_care.csv"
-    df = process_reviews(processed_file, 100)
+    df = process_reviews(health_reviews, 100)
     assert len(df) == 100
     assert 'images'not in df.columns
     assert df['text'].isnull().sum() == 0
     assert df['rating'].isnull().sum() == 0
     assert df['helpful_vote'].isnull().sum() == 0
-    assert df['title'].isnull().sum() == 0
+    assert df['product_title'].isnull().sum() == 0
     assert os.path.exists(processed_file)
     os.remove(processed_file)
 
+def test_clean_metadata(sample_meta_df):
+    test_meta_path = "meta_Health_and_Personal_Care.jsonl.gz"
 
+    df = clean_metadata(sample_meta_df)
+
+    for col in ['parent_asin', 'description', 'title', 'features']:
+        assert col in df.columns
+        assert df[col].isnull().sum() == 0
+
+def test_product_listing(sample_meta_df):
+    meta_df = clean_metadata(sample_meta_df)
+    assert (meta_df['product_listing'].str.strip().str.len() < 10).sum() == 0
