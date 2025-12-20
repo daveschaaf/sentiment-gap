33bb6bb6153765c3416bc1f107f9d3ce450c8a96
David Schaaf <davidschaaf@berkeley.edu>
2025-12-19 20:25:43 -0500

    constants defined

---
+++ src/constants.py
@@ -0,0 +1,21 @@
+MIN_CHAR_LENGTH = 3
+
+REVIEW_DATA_TYPES = {
+    'rating': 'float32',
+    'title': 'string',
+    'text': 'string',
+    'asin': 'category',
+    'parent_asin': 'category',
+    'user_id': 'string',
+    'helpful_vote': 'int32', 
+    'verified_purchase': 'bool'
+}
+
+META_DATA_TYPES = {
+    'parent_asin': 'category',
+    'title': 'string',
+    'description': 'string', 
+    'features': 'string'
+}
+
+
